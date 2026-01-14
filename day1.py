19f2663cb961bc17d6c3b83af4d0a9aa174bc51a
kmink-lab <camcufre.dev@gmail.com>
2026-01-14 13:22:22 -0300

    Recordatorio

--- day1.py
+++ day1.py
@@ -6,3 +6,11 @@ altura = 1.77
 es_backend = True
 
 print(nombre, edad, altura, es_backend)
+
+## Problemas de hoy:
+##
+## Fue dificil pushear el contenido a mi repositorio, porque la cuenta que yo estaba logueando no coincidia
+## con la cuenta que tenia logueadas en las credenciales de Windows: estaban LAS DOS.
+## Lo cual creó el problema de que al intentar pushear habia conflicto como si quisiera desde la cuenta A
+## pushear algo a la cuenta B como usuario distinto (que tecnicamente asi era, aunque no fuera lo que yo queria)
+## Resolvi eliminando la cuenta A desde el administrador de credenciales y continuando asi con el push satisfactorio.
